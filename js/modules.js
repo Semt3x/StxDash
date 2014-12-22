6db85e74226f91a92e1196bcdcd8311708bd8f80
Yoann Menelet <yoann.menelet@gmail.com>
2014-12-22 01:39:10 +0100

    Added some js code for a terminal part
    http://terminal.jcubic.pl/examples.php

--- js/modules.js
+++ js/modules.js
@@ -1,6 +1,8 @@
 // reset all modules on page load
 function reloadModules()
-{
+{    
+    $('body').css('background-color', '#fff');
+    
     setNavButtons();
 }
 
@@ -15,4 +17,30 @@ function setNavButtons()
 function navButton()
 {
     $('#mainView').load('/partials/'+$(this).attr('app')+'.html');
+}
+
+function bashMode()
+{    
+    $('body').css('background-color', '#323340');
+    
+    jQuery(function($, undefined) {
+    $('#bashArea').terminal(function(command, term) {
+        if (command !== '') {
+            try {
+                var result = window.eval(command);
+                if (result !== undefined) {
+                    term.echo(new String(result));
+                }
+            } catch(e) {
+                term.error(new String(e));
+            }
+        } else {
+           term.echo('');
+        }
+    }, {
+        greetings: "This is a simple jquery prompt\nTry some js stuff here\nCould've done my own but I've been lazy...",
+        name: 'haiJS',
+        height: 350,
+        prompt: 'js> '});
+});
 }
